49e9a1a13d7c873d7d2e1252b35a0a1a2a17ace0
covobon <doxuancuong96@gmail.com>
2019-06-30 09:01:06 +0700

    1

---
+++ src/main/java/com/smartshop/app/App.java
@@ -0,0 +1,16 @@
+package com.smartshop.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+
+@ComponentScan({"com.smartshop.security", "com.smartshop.controller"})
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
