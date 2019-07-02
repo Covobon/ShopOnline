9f61befc28def395e6c33fcd466e74ea085c7e2d
covobon <doxuancuong96@gmail.com>
2019-07-02 20:49:06 +0700

    1

--- src/main/java/com/smartshop/api/UserController.java
+++ src/main/java/com/smartshop/api/UserController.java
@@ -1,9 +1,5 @@
 package com.smartshop.api;
 
-import com.smartshop.dao.user.*;
-import com.smartshop.service.user.UserService;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
