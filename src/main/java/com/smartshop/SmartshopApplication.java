2fddfe12eeed58a9f2c1bba2f7b1e0179f0602f6
covobon <doxuancuong96@gmail.com>
2019-07-11 08:31:38 +0700

    1

--- src/main/java/com/smartshop/SmartshopApplication.java
+++ src/main/java/com/smartshop/SmartshopApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartshopApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SmartshopApplication.class, args);
 	}
-
 }
