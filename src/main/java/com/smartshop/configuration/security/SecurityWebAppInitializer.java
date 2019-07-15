9ceb414f56f1ba86fd0b0fcd19485968b0c0a2e4
covobon <doxuancuong96@gmail.com>
2019-07-15 07:42:50 +0700

    11

--- src/main/java/com/smartshop/configuration/security/SecurityWebAppInitializer.java
+++
@@ -1,6 +0,0 @@
-package com.smartshop.configuration.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
-}
