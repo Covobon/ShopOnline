523a4d990f77312e8953c61614f78e6aec78cb29
covobon <doxuancuong96@gmail.com>
2019-07-16 16:51:39 +0700

    add angular

--- src/main/java/com/smartshop/api/UserController.java
+++ src/main/java/com/smartshop/api/UserController.java
@@ -2,20 +2,28 @@ package com.smartshop.api;
 
 import com.smartshop.model.User;
 import com.smartshop.service.UserService;
+import com.smartshop.utils.EncrytedPasswordUtils;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private EncrytedPasswordUtils encrytedPasswordUtils;
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
@@ -37,5 +45,21 @@ public class UserController {
     @DeleteMapping()
     public void deleteUser(@RequestParam String userName){
         userService.deleteByUserName(userName);
+    }
+
+    @PostMapping("/authenticate")
+    @CrossOrigin
+    public ResponseEntity<User> authenticate(@RequestBody User user){
+        User theUser =  userService.findByUserName(user.getUserName());
+        if (theUser == null) {
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+
+        if (BCrypt.checkpw(user.getPassword(), theUser.getPassword())) {
+            theUser.setPassword(user.getPassword());
+            return new ResponseEntity<User>(theUser, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<User>(HttpStatus.UNAUTHORIZED);
+        }
     }
 }
