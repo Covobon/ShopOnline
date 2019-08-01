9370e43099c3832cd7c69750c45c13cce0f6cc75
covobon <doxuancuong96@gmail.com>
2019-08-01 09:27:21 +0700

    no message

--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -1,22 +1,29 @@
 import {Component, OnInit} from '@angular/core';
-import { first } from 'rxjs/operators';
+import {first} from 'rxjs/operators';
 
-import { User } from '@app/_models/user';
-import { UserService } from '@app/_services/user.service';
+import {User} from '@app/_models/user';
+import {UserService} from '@app/_services/user.service';
 import {ProductService} from '@app/_services/product.service';
 import {Product} from '@app/_models/product';
 import {environment} from '@environments/environment';
 
-@Component({ templateUrl: 'home.component.html' })
+@Component({templateUrl: 'home.component.html'})
 export class HomeComponent implements OnInit {
   loading = false;
   products: Product[];
+  page: number;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {
+  }
 
   ngOnInit() {
     this.loading = true;
-    this.productService.find(`${environment.apiUrl}/api/product`)
-      .subscribe(data => this.products = data);
+  }
+
+  loadPage(page: number) {
+    this.page = Math.ceil(page);
+    this.productService.find(`${environment.apiUrl}/api/product?page=${this.page}&pageSize=10`).subscribe(data => {
+      this.products = data;
+    });
   }
 }
